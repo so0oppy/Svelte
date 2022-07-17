14d6c424cf8a2c1a4982135b8e5bd447016b59f4
Subin <80036437+hansb1023@users.noreply.github.com>
2022-07-17 17:33:10 +0900

    220713 commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import {button} from '@src/components/button';
+    
+    const textNumbers: string[] =
+        ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']; 
+</script>
+
+<div class="frame">
+    {#each textNumbers as text}
+        <button {text} color="orange" onclick={(value) => alert{value}}></button>
+    {/each}
+</div>
+
+<style lang="scss">
+    .frame{
+        width:240px;
+        display: flex;
+        flex-wrap: wrap;
+    }
+</style>
