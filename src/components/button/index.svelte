32dc74b07c6080eb2c14423ae576352582e9b89b
Subin <80036437+hansb1023@users.noreply.github.com>
2022-07-17 18:34:12 +0900

    change svelte

--- src/components/button/index.svelte
+++ src/components/button/index.svelte
@@ -1,25 +1,30 @@
 <script lang="ts">
-    export let text : string;
-    export let onClick: (value : string) => void;
-    export let color: 'orange' | 'grey' | 'dark-grey' ;
+    export let text : string; //함수타입
+    export let onClick : (value: string)=> void;
+    export let color : 'orange' | 'grey' | 'dark-grey';
 </script>
-
 <div class="button" 
 data-color={color} 
-onclick={() => onClick(text)}> {text} </div>
+on:click={()=>onClick(text)}>{text}</div>
 
 <style lang="scss">
     .button {
-        width:80px;
+        width: 80px;
         height: 80px;
-        display: flex;
+        display : flex;
         align-items: center;
         justify-content: center;
         font-size: 24px;
         font-weight: 600;
 
-        &{data-color:'orange'; background-color: orange;}
-        &{data-color:'grey'; background-color: grey;}
-        &{data-color:'dark-grey'; background-color: darkgray;}
+        &[data-color='orange'] {
+            background-color: orange;
+        }
+        &[data-color='grey'] {
+            background-color: grey;
+        }
+        &[data-color='dark-grey'] {
+            background-color: darkgrey;
+        }
     }
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-    import {button} from '@src/components/button';
-    
-    const textNumbers: string[] =
-        ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']; 
-</script>
+    import { Button } from '@src/components/button';
 
-<div class="frame">
-    {#each textNumbers as text}
-        <button {text} color="orange" onclick={(value) => alert{value}}></button>
-    {/each}
-</div>
+    const textNumbers : string[] = ['1', '2', '3', '4', '5', '6', '7', '8', '9']; 
+    
+</script>
+    <div class ="frame">
+        {#each textNumbers as text}
+        <Button {text} color="orange" onClick={(value) => alert(value)} />
+            {/each}
+    </div>
 
 <style lang="scss">
-    .frame{
-        width:240px;
+    .frame {
+        width: 240px;
         display: flex;
         flex-wrap: wrap;
     }
 </style>
+
