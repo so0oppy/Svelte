f2f8e7a472e2a18dd40a8fd58c92f188233b513a
Subin <80036437+hansb1023@users.noreply.github.com>
2022-07-26 17:23:06 +0900

    change

---
+++ my-app/src/components/button/index.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    export let text : string; //함수타입
+    export let onClick : (value: string)=> void;
+    export let color : 'orange' | 'grey' | 'dark-grey';
+</script>
+
+<div class="button" data-color={color} on:click={()=>onClick(text)}>{text}</div>
+
+<style lang="scss">
+    .button {
+        width: 80px;
+        height: 80px;
+        display : flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 20px;
+        font-weight: 600;
+
+        &[data-color='orange'] {
+            background-color: orange;
+        }
+        &[data-color='grey'] {
+            background-color: grey;
+        }
+        &[data-color='dark-grey'] {
+            background-color: darkgrey;
+        }
+    }
+</style>
